f2f8c674909bd1d9b4a48e3b73ae2b280cdf1612
Charles Wong <charleswonghm@gmail.com>
2021-12-10 23:25:47 -0500

    Create bitmap output

---
+++ src/bitmap.h
@@ -0,0 +1,84 @@
+#ifndef RTIAW_SRC_BITMAP_H_
+#define RTIAW_SRC_BITMAP_H_
+
+#include <stdint.h>
+
+namespace bitmap {
+
+	const uint8_t BMP_HEADER_SIZE = 14;
+	const uint8_t DIB_HEADER_SIZE = 108;
+
+	struct Rgba {
+		uint8_t b, g, r, a;
+
+		Rgba() {}
+
+		Rgba(uint8_t r, uint8_t g, uint8_t b, uint8_t a) :
+			r(r), g(g), b(b), a(a){
+		}
+	};
+
+#pragma pack(push, 1)
+	struct BMPHeader {
+		uint16_t type;
+		uint32_t size;
+		uint16_t reserved1;
+		uint16_t reserved2;
+		uint32_t offset;
+
+		BMPHeader(uint32_t size, uint32_t offset) :
+			type(0x4D42), size(size), reserved1(0), reserved2(0), offset(offset) {
+		}
+
+	};
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+	struct DIBHeader {
+		uint32_t headerSize;
+
+		uint32_t bmpWidth;
+		uint32_t bmpHeight;
+
+		uint16_t colorPlane;
+		uint16_t colorDepth;
+		uint32_t compression;
+
+		uint32_t rawSize;
+
+		uint32_t resHorizontal;
+		uint32_t resVertical;
+
+		uint32_t numColors;
+		uint32_t importantColors;
+
+		uint32_t rBitMask;
+		uint32_t gBitMask;
+		uint32_t bBitMask;
+		uint32_t aBitMask;
+
+		uint32_t lcsWinColorSpace;
+		uint32_t reserved1[9];
+		uint32_t reserved2;
+		uint32_t reserved3;
+		uint32_t reserved4;
+
+		DIBHeader(uint32_t bmpWidth, uint32_t bmpHeight, uint32_t dpiHorizontal, uint32_t dpiVertical) :
+			headerSize(108),
+			bmpWidth(bmpWidth), bmpHeight(bmpHeight),
+			colorPlane(1), colorDepth(32), compression(3),
+			numColors(0), importantColors(0),
+			aBitMask(0xFF000000), rBitMask(0x00FF0000), gBitMask(0x0000FF00), bBitMask(0x000000FF),
+			lcsWinColorSpace(0x57696E20), // little endian "win"
+			reserved2(0), reserved3(0), reserved4(0) {
+
+			rawSize = bmpWidth * bmpHeight * 4;
+
+			resHorizontal = (uint32_t) floor(dpiHorizontal * 39.3701 + 0.5);
+			resVertical = (uint32_t) floor(dpiVertical * 39.3701 + 0.5);
+		}
+	};
+#pragma pack(pop)
+
+}
+#endif // !RTIAW_SRC_BITMAP_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,44 @@
 ﻿// main.cpp: Entry point of application
 //
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "bitmap.h"
+
 int main()
 {
-	
+	const int imageX = 256;
+	const int imageY = 256;
+	int totalHeaderSize = bitmap::BMP_HEADER_SIZE + bitmap::DIB_HEADER_SIZE;
+
+	bitmap::BMPHeader bmpHeader(totalHeaderSize + imageX * imageY * 4, totalHeaderSize);
+	bitmap::DIBHeader dibHeader(imageX, imageY, 300, 300);
+
+	std::vector<bitmap::Rgba> imageBuffer(imageX * imageY); // buffer is read from bottom left to top right
+
+	for (int j = imageY - 1; j >= 0; j--) {
+		for (int i = 0; i < imageX; i++) {
+
+			uint32_t r = (uint32_t) (255.9999 * (double(i) / (imageX - 1)));
+			uint32_t g = (uint32_t) (255.9999 * (double(j) / (imageY - 1)));
+			uint32_t b = (uint32_t) (255.9999 * 0.25);
+			
+			imageBuffer[j * imageY + i] = bitmap::Rgba(r, g, b, 0xFF);
+		}
+	}
+
+	std::ofstream output("output.bmp", std::ios::binary | std::ios::trunc);
+
+	if (output.is_open())
+	{
+		output.write((char*)&bmpHeader, bitmap::BMP_HEADER_SIZE);
+		output.write((char*)&dibHeader, bitmap::DIB_HEADER_SIZE);
+
+		output.write((char*)&imageBuffer[0], imageX * imageY * 4);
+
+		output.close();
+	}
+
+	return 0;
 }
